23e8dbe2a7080915a6905a0e00b8e3c1b54a6c92
follsqq01 <ruslanmelkumian80@yandex.ru>
2025-02-16 23:06:31 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-
+// 1 задание
 const age = prompt("Введите ваш возраст:");
 if (age < 65) {
     alert("Вам ещё рано на пенсию");
@@ -6,7 +6,7 @@ if (age < 65) {
     alert("Поздравляем с пенсионным возрастом!");
 }
 
-
+// 2 задание
 const num1 = prompt("Введите первое число:");
 const num2 = prompt("Введите второе число:");
 
@@ -18,7 +18,7 @@ if (num1 > num2) {
     alert("Числа равны");
 }
 
-
+// 3 задание
 const number = prompt("Введите число:");
 if (number % 2 === 0) {
     alert("Это чётное число");
@@ -26,7 +26,7 @@ if (number % 2 === 0) {
     alert("Это нечётное число");
 }
 
-
+// 4 задание
 if (number > 100) {
     alert("Большое число");
 } else if (number < 100) {
@@ -34,7 +34,7 @@ if (number > 100) {
 } else {
     alert("Точно 100!");
 }
-
+// 5 задание
 const login = prompt("Введите логин:");
 const password = prompt("Введите пароль:");
 if (login === "admin" && password === "12345") {
@@ -42,7 +42,7 @@ if (login === "admin" && password === "12345") {
 } else {
     alert("Неверный логин или пароль");
 }
-
+// 6 задание
 const year = prompt("Введите год:");
 
 if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
@@ -50,7 +50,7 @@ if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
 } else {
     alert("Это не високосный год");
 }
-
+// 7 задание
 const secretNumber = 7;
 const userGuess = prompt("Угадайте число от 1 до 10:");
 
